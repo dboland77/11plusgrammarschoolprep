28cc634efb302d3969733bc4a7ba7c60f6202d57
dboland77 <dboland77@gmail.com>
2025-09-24 14:59:10 +0100

    removed tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Suspense } from 'react';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
-      <body className='min-h-screen bg-slate-50 flex items-center justify-center'>
+      <body className=''>
         <Suspense fallback={<div>Loading ...</div>}>{children}</Suspense>
       </body>
     </html>
